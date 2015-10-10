4fe683cd014a22123e97f684d8c6993391ba1403
Abhijit Tripathy <atripath@ucsd.edu>
2015-10-10 06:09:55 -0700

    final code for assignment-2 ultrasonic interrupt

--- ultrasonic-intr.cpp
+++ ultrasonic-intr.cpp
@@ -1,26 +1,26 @@
 #include "mbed.h"
  
-InterruptIn echopin(p6); 
-DigitalOut triggerpin(p25); 
+InterruptIn echopin(p6); // Interrupting the echo pulse pin
+DigitalOut triggerpin(p25); // Pin for providing trigger pulse
 Serial terminal(USBTX, USBRX);
 Timer timer;
 
 
-
+// Takes duration of echo pulse width in us and returns distance from sensor in cms
 float cal_distance(float duration)
 {
-    return (duration / 58.0);  
+    return (duration / 58.0);  // d = (100*340*t/2)*(10^-6) ~ t/58
 }
 
-void timer_on() 
+void timer_on() // ISR for rising edge
 {
-    timer.reset(); 
+    timer.reset(); // On detecting rising edge , we start the timer
     timer.start();
 }
 
-void timer_off() 
+void timer_off() // ISR for falling edge 
 {
-    timer.stop(); 
+    timer.stop(); // On detecting falling edge , we stop timer
     float duration = (float)(timer.read_us()); 
     
     float distance = cal_distance(duration);
@@ -29,8 +29,8 @@ void timer_off()
 
 int main()
 {    
-    echopin.rise(&timer_on); 
-    echopin.fall(&timer_off); 
+    echopin.rise(&timer_on); // Interrupt for rising edge 
+    echopin.fall(&timer_off); // Interrupt for falling edge
     
     while(1) 
     {        
